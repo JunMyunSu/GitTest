af5990407a5fea8526802d2e696b64477d99ad04
Jun <metalbaram@naver.com>
2023-01-19 13:46:58 +0900

    first commit

--- src/main/java/org/example/test1.java
+++ src/main/java/org/example/test1.java
@@ -2,5 +2,6 @@ package org.example;
 public class test1 {
     public static void main(String...arg) {
         System.out.println("branch test");
+        System.out.println("branch merge");
     }
 }
